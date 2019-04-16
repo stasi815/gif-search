e869850d220d8736f674fdf3325f1fc76bacb071
DUC Mobile Mac Lab <ducmobilemaclab@DUCs-MacBook-Pro-10A.local>
2019-04-15 19:40:06 -0700

    Completed Project/Final Commit

--- app.js
+++ app.js
@@ -1,16 +1,55 @@
 // Require Libraries
 const express = require('express');
 
+// Require HTTP MODULE
+const http = require('http');
+
+//Require Giphy Library
 const giphy = require('giphy-api')();
+
+// Middleware
+const exphbs  = require('express-handlebars');
+
 
 // App Setup
 const app = express();
 
 // Middleware
-const exphbs  = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+//Public Folder Styles
+app.use(express.static('public'));
+
+//HTTP Route
+app.get('/', (req, res) => {
+  console.log(req.query.term)
+  const queryString = req.query.term;
+  // ENCODE THE QUERY STRING TO REMOVE WHITE SPACES AND RESTRICTED CHARACTERS
+  const term = encodeURIComponent(queryString);
+  // PUT THE SEARCH TERM INTO THE GIPHY API SEARCH URL
+  const url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
+
+  http.get(url, (response) => {
+    // SET ENCODING OF RESPONSE TO UTF8
+    response.setEncoding('utf8');
+
+    var body = '';
+
+    response.on('data', (d) => {
+      // CONTINUOUSLY UPDATE STREAM WITH DATA FROM GIPHY
+      body += d;
+    });
+
+    response.on('end', () => {
+      // WHEN DATA IS FULLY RECEIVED PARSE INTO JSON
+      const gifs = JSON.parse(body).data
+      // RENDER THE HOME TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
+      res.render('home', { gifs });
+    });
+  });
+});
 
 // Routes
 
@@ -33,9 +72,6 @@ app.get('/', (req, res) => { // => "{ term: hey" }
   res.render('home')
 });
 
-app.get('/', (req, res) => {
-  res.render('home')
-});
 
 // GIPHY API Route
 
